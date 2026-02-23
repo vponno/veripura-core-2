83cde58c04877a597900d3fe7972b67cd5abbf7d
Onno Stienen <onno@Onnos-Mac-mini-2.local>
2026-02-23 18:28:08 +0700

    fix: change storagebucket back to correct firebase storage bucket

--- services/lib/firebase.ts
+++ services/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "YOUR_API_KEY",
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "veripura-f92a5.firebaseapp.com",
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "veripura-f92a5",
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "veripura-f92a5-uploads",
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "veripura-f92a5.firebasestorage.app",
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "487093021373",
     appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:487093021373:web:14c39cc3a58196621da5d4",
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-QY4T240SHL"
